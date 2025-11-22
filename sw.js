2f2737270c9d3b13700c5bc391a8956463ea8a0e
Ben Hammond <benjamin.hammond@gmail.com>
2025-11-21 17:23:53 -0700

    feat(serviceWorker): add service worker for caching and updates

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_VERSION = '2024-11-21-v1';
+const CACHE_NAME = 'synthy-sisters-' + CACHE_VERSION;
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing:', CACHE_NAME);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating:', CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName.startsWith('synthy-sisters-') && cacheName !== CACHE_NAME) {
+            console.log('Clearing old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
